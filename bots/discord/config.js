d32827f451542644acb1bdd6c40d660094106f18
Dave KE <69384676+daveh566@users.noreply.github.com>
2024-02-15 16:41:45 +0300

    Update config.js

--- bots/discord/config.js
+++ bots/discord/config.js
@@ -2,7 +2,7 @@ module.exports = {
     apiurl: 'http://localhost:1337',
     apipassword: '',
 
-    discordtoken: '',
+    discordtoken: 'MTIwNzY4MDUwNjc2Mjc2MDI5Mw.GplK0d.t0428it_HEN6fFIOo6qsYq_DTPb5mj5EyFbp5E',
     discordprefix: '!',
     secretpassword: 'ChAnGeThIsWiThYoUrPaSsWoRdNoW!',
     botuser_rolename: 'Bot User',
